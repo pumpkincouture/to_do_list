489a48f315ffa0fcb9eb9c3379c506df849e01c1
pumpkincouture <pumpkincouture@gmail.com>
2014-11-22 11:50:54 -0600

    Updated index spec

--- spec/features/todo_items/index_spec.rb
+++ spec/features/todo_items/index_spec.rb
@@ -4,5 +4,9 @@ describe "Viewing todo items" do
 	let(:todo_list) { ToDo.create(title: "Grocery list", description: "Groceries") }
 	it "displays no items when todo list is empty" do 
 		visit "/to_dos"
+		within '#todo_list_#{todo_list.id}' do 
+			click_link "List Items"
+		end
+		expect(page).to have_content("TodoItems#index")
 	end
 end
